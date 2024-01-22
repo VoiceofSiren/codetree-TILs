37cb70c3cf56c7787d2d39adccb234859f5e85d4
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-01-22 17:30:13 +0900

    Title: 나눗셈의 나머지, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/나눗셈의 나머지/remainder-of-division.py
+++ 240122/나눗셈의 나머지/remainder-of-division.py
@@ -3,8 +3,9 @@ a, b = tuple(map(int, input().split()))
 resultList = [0 for _ in range(b)]
 
 while a > 1:
-    resultList[int(a % b)] += 1
-    a /= b
+    resultList[a % b] += 1
+    # print(resultList)
+    a = a // b
 
 sumVal = 0
 for i in resultList:
