8270fe9a52eaced1f0750f7d2f9630202855c4f7
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-01-22 16:51:34 +0900

    Title: 십의 자리 숫자, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240122/십의 자리 숫자/number-of-tens-digit.py
@@ -0,0 +1,8 @@
+inputList = list(map(int, input().split()))
+
+resultList = [0 for _ in range(10)]
+
+for i in inputList:
+    resultList[i//10] += 1
+for i in range(1, 10):
+    print(f'{i} - {resultList[i]}')
