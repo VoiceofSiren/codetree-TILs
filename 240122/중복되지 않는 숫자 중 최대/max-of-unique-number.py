a07856da28c336222c81f126d931df3deb1bcc9c
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-01-22 18:19:45 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240122/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240122/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -3,12 +3,12 @@ import sys
 n = int(input())
 inputList = list(map(int, input().split()))
 
-resultList = [0 for _ in range(max(inputList))]
+resultList = [0 for _ in range(1000)]
 
 for i in inputList:
     resultList[i] += 1
 #print(resultList)
-filteredList = [0 for _ in range(max(inputList))]
+filteredList = [0 for _ in range(1000)]
 
 for i in range(len(resultList)):
     if resultList[i] >= 2:
@@ -17,13 +17,13 @@ for i in range(len(resultList)):
         filteredList[i] = resultList[i]
 #print(filteredList)
 max_value, cnt_value = -sys.maxsize, 0
-for i in filteredList:
-    if i != 0:
-        cnt_value += 1
 
 for i in range(len(filteredList)):
     if filteredList[i] == 1:
         max_value = i
+    if filteredList[i] == 0:
+        cnt_value += 1
+
 if cnt_value == 0:
     print(-1)
 else:
