97958479937e19bb646c7977b68538b465b4f675
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-01-25 10:45:11 +0900

    Title: 최고의 33위치, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240125/최고의 33위치/best-place-of-33.py
+++ 240125/최고의 33위치/best-place-of-33.py
@@ -1,19 +1,20 @@
 n = int(input())
 square = [list(map(int, input().split())) for _ in range(n)]
 
-def count_coins(i, j, square):
+def count_coins(row1, col1, row2, col2):
     count_value = 0
-    for i in range(3):
-        for j in range(3):
-            if square[i][j] == 1:
-                count_value += 1
+    for row in range(row1, row2 + 1):
+        for col in range(col1, col2 + 1):
+            count_value += square[row][col]
     return count_value
 
 
 max_value = 0
 for i in range(n-2):
     for j in range(n-2):
-        coin_value = count_coins(i, j, square)
+        if i + 2 >= n or j + 2 >= n:
+            continue
+        coin_value = count_coins(i, j, i+2, j+2)
         if coin_value > max_value:
             max_value = coin_value
 print(max_value)
