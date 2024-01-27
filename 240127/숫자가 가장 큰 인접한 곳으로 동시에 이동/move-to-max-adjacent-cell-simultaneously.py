847216fbdcde0152c7e496e187accd4bb9835439
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-01-27 21:06:12 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 229ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240127/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -0,0 +1,71 @@
+n, m, t = tuple(map(int, input().split()))
+
+square = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+temp_count = [
+    [0 for j in range(n)]
+    for i in range(n)
+]
+
+count = [
+    [0 for j in range(n)]
+    for i in range(n)
+]
+
+#      N  S  W  E
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def get_max_value(x, y, square):
+    max_value = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if not in_range(nx, ny):
+            continue
+        max_value = max(max_value, square[nx][ny])
+
+def get_dir_num(x, y, square):
+    dir_num = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if not in_range(nx, ny):
+            continue
+        if square[nx][ny] == get_max_value(x, y, square):
+            dir_num = i
+            break
+    return dir_num
+
+def move(x, y, square):
+    temp_value = temp_count[x][y]
+    temp_count[x][y] = 0
+    x = x + dx[get_dir_num(x, y, square)]
+    y = y + dy[get_dir_num(x, y, square)]
+    temp_count[x][y] += temp_value
+
+
+rc_list = []
+for _ in range(m):
+    r, c = tuple(map(int, input().split()))
+    r, c = r - 1, c - 1
+    rc_list.append([r, c])
+    temp_count[r][c] = 1
+
+for _ in range(t):
+    for i in range(m):
+        x, y = rc_list[i][0], rc_list[i][1]
+        move(x, y, square)
+
+result = 0
+for row in temp_count:
+    for element in row:
+        if element >= 1:
+            result += 1
+print(result)
